e08a726e9841f36596cdbe40407e22d8aa7c0d1b
snqu-526 <snqu-526@snqu-526s-Mac-mini.local>
2025-02-19 10:38:08 +0800

    add docs

---
+++ docs/config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  base: '/pxx_project/',
+  title: '技术中心软件说明',
+  description: '技术中心软件说明',
+  themeConfig: {
+    sidebar: [
+      ['/', '简介'],
+      {
+        title: '开发组',
+        collapsable: false,
+        children: [
+          ['/group/xian/', '西安组'],
+          ['/group/zhengzhou/', '郑州组'],
+          ['/group/hangzhou/', '杭州组'],
+          ['/group/shenzhen/', '深圳组']
+        ]
+      },
+      {
+        title: '软件用途',
+        collapsable: false,
+        children: [
+          ['/purpose/qywz/', '企业网站'],
+          ['/purpose/ltsj/', '论坛社交'],
+          ['/purpose/qyyy/', '企业应用'],
+          ['/purpose/yxhd/', '营销活动']
+        ]
+      }
+    ]
+  },
+  sidebarDepth: 2
+}
